ab00a76836988513a40fbdf9eb0ae6f7f0dd5083
furkankocyigit <furkankocyigit58@gmail.com>
2023-03-07 23:03:37 +0300

    cracking-coding-interview solution project is initialized

---
+++ CrackingCoding/Program.cs
@@ -0,0 +1,15 @@
+﻿using CrackingCoding.Chapters.ArraysStrings;
+
+namespace CrackingCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+            ArraysAndStringSolutions a = new ArraysAndStringSolutions();
+        }
+    }
+}
