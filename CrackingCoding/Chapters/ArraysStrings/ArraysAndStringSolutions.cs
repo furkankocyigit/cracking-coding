6c239e72b5c9e9c5a220bf2945a9cb1454e2b32f
furkankocyigit <furkankocyigit58@gmail.com>
2023-03-08 09:28:58 +0300

    isStringUniq solution is added with tests

--- CrackingCoding/Chapters/ArraysStrings/ArraysAndStringSolutions.cs
+++ CrackingCoding/Chapters/ArraysStrings/ArraysAndStringSolutions.cs
@@ -2,6 +2,37 @@
 {
     public class ArraysAndStringSolutions
     {
+        public bool isStringUniq(string input)
+        {
+            bool isUniq = true;
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        isUniq = false;
+                    }
+                }
+            }
+            return isUniq;
+        }
+
+        public bool isStringUniqSecondSolution(string input)
+        {
+            bool isUniq = true;
+            bool[] chars = new bool[128];
+            for (int i = 0; i < input.Length; i++)
+            {
+                var val = input.ElementAt(i);
+                if (chars[val])
+                {
+                    isUniq = false;
+                }
+                chars[val] = true;
+            }
+            return isUniq;
+        }
     }
 }
