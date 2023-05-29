6bd5e343b27b1d369ba20919dd0bb0775f40442c
furkankocyigit <furkankocyigit58@gmail.com>
2023-05-29 10:47:45 +0300

    bit insertion method is added

---
+++ CrackingCoding/Chapters/BitManipulation/BitManipulation.cs
@@ -0,0 +1,21 @@
+﻿namespace CrackingCoding.Chapters.BitManipulation
+{
+    public static class BitManipulation
+    {
+        public static int insertValue(int source, int value, int i, int j)
+        {
+            if (value > source) return 0;
+            if (i > j) return 0;
+
+            int allOnes = ~0;
+            int left = allOnes << j + 1;
+            int right = (1 << i) - 1;
+            int mask = left | right;
+            source = source & mask;
+
+            source = source | (value << i);
+
+            return source;
+        }
+    }
+}
